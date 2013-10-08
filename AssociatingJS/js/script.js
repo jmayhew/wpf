66e61bf54e12b754b7781544e6abd045f93b050b
jmayhew <jessemayhew@fullsail.edu>
2013-10-08 12:12:59 -0400

    arithmetic assignment opertator

--- AssociatingJS/js/script.js
+++ AssociatingJS/js/script.js
@@ -1,13 +1,13 @@
 //alert("Hello World");
 //Jesse Mayhew October 4 2013 Values and Variables
 //variables of different types numbers, boolean, and strings
-var age = 5;//number variable 
+/*var age = 5;//number variable 
 
 var isStudent = false;//boolean variable
 
 var firstName = "Jes";
 
-var phrase = 'I don\'t know you\'ll \n have to talk to the professor';
+var phrase = 'I don\'t know you\'ll \n have to talk to the professor';*/
 
 //arrays
 //var soccerTeams = ["Italy", "Brasil", "Germany"]; //0,1,2,3
@@ -35,4 +35,8 @@ console.log(yearBorn);//printing both in console*/
 
 //var isStudent = false;//boolean variable
 //var myFriend = 
-/var firstName = "jesse";
+//var firstName = "jesse";
+var a = 2;//sets up variable 
+
+a = a + 3;
+console.log(a);
