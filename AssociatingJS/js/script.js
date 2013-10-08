113a7af30a48d4bb8acd3b2798b6f75daa5ee744
jmayhew <jessemayhew@fullsail.edu>
2013-10-08 10:29:43 -0400

    arrays soccer teams setting up assignment expressions

--- AssociatingJS/js/script.js
+++ AssociatingJS/js/script.js
@@ -1,1 +1,15 @@
-alert("Hello World");
+//alert("Hello World");
+//Jesse Mayhew October 4 2013 Values and Variables
+//variables of different types numbers, boolean, and strings
+//var age = 5;//number variable 
+
+//var isStudent = false;//boolean variable
+
+//var firstName = "Jes";
+
+//var phrase = 'I don/'t know you/'ll have to talk to the professor';
+
+//arrays
+var soccerTeams = ["Italy", "Brasil", "Germany"]; //0,1,2,3
+
+console.log(soccerTeams);
---
+++ Expressions_worksheet/expressions.js
@@ -0,0 +1,6 @@
+//Jes Mayhew  October 6 2013  Assignment Expressions
+var sparkyAge = 
+
+var humanYears =
+
+var sparkyAge = (human years*7)
