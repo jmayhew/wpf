ca1b931417a9d531c58d903b4f3e9698b02ac6f4
jmayhew <jessemayhew@fullsail.edu>
2013-10-08 11:37:25 -0400

    printing out variable in console

--- AssociatingJS/js/script.js
+++ AssociatingJS/js/script.js
@@ -10,14 +10,17 @@
 //var phrase = 'I don/'t know you/'ll have to talk to the professor';
 
 //arrays
-var soccerTeams = ["Italy", "Brasil", "Germany"]; //0,1,2,3
+//var soccerTeams = ["Italy", "Brasil", "Germany"]; //0,1,2,3
 
 //console.log(soccerTeams[3]);//undefined- no index number three
 
-soccerTeams[1] = 'Germany';//change index number one to Germany
-soccerTeams[3] = 'Argentina';
+//soccerTeams[1] = 'Germany';//change index number one to Germany
+//soccerTeams[3] = 'Argentina';
 
- var num = 2;// using variable to see what is in index number two
+// var num = 2;// using variable to see what is in index number two
  
-console.log(soccerTeams[num]);//lets me see whats in index number one
+//console.log(soccerTeams[num]);//lets me see whats in index number one
 
+var yearBorn = 1955;
+
+console.log(yearBorn);
