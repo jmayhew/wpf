e80bfc0300e34dd2c67ae1dbeb57548803cefea1
jmayhew <jessemayhew@fullsail.edu>
2013-10-09 10:43:35 -0400

    expressions worksheet

--- Expressions_worksheet/expressions.js
+++
@@ -1,6 +0,0 @@
-//Jes Mayhew  October 6 2013  Assignment Expressions
-var sparkyAge = 
-
-var humanYears =
-
-var sparkyAge = (human years*7)
---
+++ Expressions_worksheet/js/expressions.js
@@ -0,0 +1,7 @@
+//Jes Mayhew  October 6 2013  Assignment Expressions
+var sparkyAge = 7;
+
+var humanYears = 1;
+
+var result = (humanYears*sparkyAge)
+console.log(result);
