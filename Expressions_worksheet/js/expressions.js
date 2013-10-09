26386844e47dc5988560bd570a4efb5ea48b9af7
jmayhew <jessemayhew@fullsail.edu>
2013-10-09 11:30:21 -0400

    Pizza expression part one

--- Expressions_worksheet/js/expressions.js
+++ Expressions_worksheet/js/expressions.js
@@ -1,8 +1,14 @@
 //Jes Mayhew  October 6 2013  Assignment Expressions
-var sparkyAge = 7;
+/*var sparkyAge = 7;//variable for Sparky's age
 
-var humanYears = 1;
+var humanYears = 1;//variable for how many human years Sparky has been alive
 
-var actualAge = (humanYears*sparkyAge)
+var actualAge = (humanYears*sparkyAge)//Sparky's actual age
 
-console.log("Sparky is "+ humanYears+" human years old which is "+ sparkyAge+" in dog years.");
+console.log("Sparky is "+ humanYears+" human years old which is "+ sparkyAge+" in dog years.");*/
+var slices = 500;//how many slices
+var people = 6;
+var pizzas = 10;
+
+var result = (slices/people);
+console.log("Each person ate "+ result+" slices of pizza at the party.")
