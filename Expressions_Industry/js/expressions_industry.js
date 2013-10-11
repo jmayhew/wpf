4c7aa7d3593f828f14d4c6695b1eefe037691134
jmayhew <jessemayhew@fullsail.edu>
2013-10-11 09:16:51 -0400

    finishing up industry

--- Expressions_Industry/js/expressions_industry.js
+++ Expressions_Industry/js/expressions_industry.js
@@ -23,23 +23,34 @@ var shotsOffTarget =0;
 var shotsOnTarget=1;// , possessionTime, shotTotal, percentOfGoal
 var possessionTime=2;
 var shotTotal=3;
-var result =4;
+ var percentAttacking=4;
+var goals = 5;
 
-var futbolArray =[];
-futbolArray[shotsOffTarget]=prompt("How many off target shots did your team have today?(Keeper did not touch nor hit post)";
+
+futbolArray = [0,1,2,3,4,5];
+futbolArray[shotsOffTarget]=prompt("How many off target shots did your team have today?(Keeper did not touch nor hit post)");
 futbolArray[shotsOnTarget]=prompt("How many shots on target were there?(Keeper touched or hit post)");
 futbolArray[possessionTime]=prompt("How many minutes did your team have possession?");
 futbolArray[shotTotal]=Number(futbolArray[shotsOffTarget])+Number(futbolArray[shotsOnTarget]);
-
+futbolArray[goals]=prompt("How many goals did your team have?");
+futbolArray[4]=prompt("Your team spent "+parseInt(percentAttacking)+"% of the possession time attacking the goal.");
 //var shotsOffTarget = prompt("How many off target shots did your team have today?(Keeper did not touch nor hit post)")
 //var shotsOnTarget = prompt("How many shots on target were there?(Keeper touched or hit post)")
-//var possessionTime =prompt("How many minutes did your team have possession?")
+//var possessionTime =prompt("How many minutes did your team have possession?")=
 
- shotTotal = Number(shotsOffTarget)+ Number(shotsOnTarget);
+ futbolArray[3] = Number(futbolArray[0])+ Number(futbolArray[1]);
+console.log("Your team shot "+futbolArray[3]+" times");
+console.log(futbolArray[3]);
+console.log(futbolArray[1]);
+ percentAttacking = (1-Number(futbolArray[3])/100) * Number(futbolArray[2]);
+console.log("Your team spent "+parseInt(percentAttacking)+"% of the possession time attacking the goal.");
 
- result= (1-Number(shotTotal)/100) * Number(possessionTime);
 
-var result = prompt("Your team spent "+parseInt(result)+"% of the possession time attacking the goal.")
+    console.log("Your team scored "+goals+" goals.");
+
+
+
+/*var result = prompt("Your team spent "+parseInt(result)+"% of the possession time attacking the goal.")
 console.log("Your team spent "+parseInt(result)+"% of the possession time attacking the goal.");
 console.log(shotsOffTarget);
 console.log(shotsOnTarget);
@@ -62,4 +73,4 @@ goals [7] = prompt("At what minute did your team score the seventh goal?");
 goals = 1;
 var totalGoals= Number(goals[0])+Number(goals)[1]+Number(goals)[2]+Number(goals)[3]+Number(goals)[4]+Number(goals)[5]+Number(goals)[6];
 console.log("Your team scored "+totalGoals+" goals during "+result+"% of attacking.");
-var goals = prompt("Your team scored "+totalGoals+" goals during "+result+"% of attacking.");
+var goals = prompt("Your team scored "+totalGoals+" goals during "+result+"% of attacking.");*/
