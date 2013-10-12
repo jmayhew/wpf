4fb44a4dda0bb913d9700c26dcf5a9568439fe93
jmayhew <jessemayhew@fullsail.edu>
2013-10-12 12:05:48 -0400

    no print out FAlSE

--- week three conditionals/js/conditionals.js
+++ week three conditionals/js/conditionals.js
@@ -2,7 +2,7 @@
  * Created by jessemayhew on 10/12/13.
  */
 //conditionals
-var oldEnough = true;
+var oldEnough = false;
 
 //if the child is old enough print to the console "you can ride"
 if(oldEnough){
