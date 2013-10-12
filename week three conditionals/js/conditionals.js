1c4e0a3b2fc0dcbd18f88400701c073e2a275cc5
jmayhew <jessemayhew@fullsail.edu>
2013-10-12 14:02:20 -0400

    if conditions

--- week three conditionals/js/conditionals.js
+++ week three conditionals/js/conditionals.js
@@ -4,7 +4,7 @@
 //conditionals
 
 //if the kid is over 48 inches in height
-var kidHeight = 50;
+/*var kidHeight = 50;
 var minHeight = 48;
 var sneakerLift = 2;
 //if the child is old enough print to the console "you can ride"
@@ -12,5 +12,17 @@ if(kidHeight+sneakerLift > minHeight ){
     //code performed if condition is true
   console.log("You can ride the Coaster");
 
-}
+}*/
 //console.log("this comes after when false");
+var kidHeight = 47;
+var minHeight = 48;
+//if the child is old enough print to the console
+//if the child is over 48 inches in height
+if(kidHeight > minHeight){
+    //code performed if condition is true
+    console.log("You can ride the Coaster");
+}
+if(kidHeight < minHeight){
+    console.log("Sorry kid you got some growing to do");
+}
+
