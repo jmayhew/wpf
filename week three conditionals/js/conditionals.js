4651ff9e944f9f5adfe8469c6a233392cd52827a
jmayhew <jessemayhew@fullsail.edu>
2013-10-12 13:54:23 -0400

    variabels in expressions

--- week three conditionals/js/conditionals.js
+++ week three conditionals/js/conditionals.js
@@ -6,8 +6,9 @@
 //if the kid is over 48 inches in height
 var kidHeight = 50;
 var minHeight = 48;
+var sneakerLift = 2;
 //if the child is old enough print to the console "you can ride"
-if(kidHeight+2 > minHeight ){
+if(kidHeight+sneakerLift > minHeight ){
     //code performed if condition is true
   console.log("You can ride the Coaster");
 
