91a1c71dfd6aa21384aa1babeb645aa95d96c626
jmayhew <jessemayhew@fullsail.edu>
2013-10-12 12:16:40 -0400

    greater than height

--- week three conditionals/js/conditionals.js
+++ week three conditionals/js/conditionals.js
@@ -2,12 +2,13 @@
  * Created by jessemayhew on 10/12/13.
  */
 //conditionals
-var oldEnough = false;
 
+//if the kid is over 48 inches in height
+var kidHeight = 30;
 //if the child is old enough print to the console "you can ride"
-if(oldEnough){
+if(kidHeight > 48){
     //code performed if condition is true
   console.log("You can ride the Coaster");
 
 }
-console.log("this comes after when false");
+//console.log("this comes after when false");
