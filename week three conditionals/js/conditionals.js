91a72ec0154d59d448e23db3bb73f7689ebb33b9
jmayhew <jessemayhew@fullsail.edu>
2013-10-12 19:27:37 -0400

    and operator

--- week three conditionals/js/conditionals.js
+++ week three conditionals/js/conditionals.js
@@ -14,7 +14,7 @@ if(kidHeight+sneakerLift > minHeight ){
 
 }*/
 //console.log("this comes after when false");
-var kidHeight = 47;
+/*var kidHeight = 47;
 var minHeight = 48;
 //if the child is old enough print to the console
 //if the child is over 48 inches in height
@@ -22,7 +22,49 @@ if(kidHeight > minHeight){
     //code performed if condition is true
     console.log("You can ride the Coaster");
 }else{
-//if(kidHeight <= minHeight){
+//substituting if(kidHeight <= minHeight){
     console.log("Sorry kid you got some growing to do");
-}
+}*/
 
+/*
+//conditional logic else if
+var kidHeight = 47;
+var minHeight = 48;
+var wParentHeight = 45;//the height of the kid w/parent
+//if the child is old enough print to the console
+//if the child is over 48 inches in height
+//plain if statements are for the computer to choose between doing a block of code or to skip it
+//"if and else" is to choose b/t two blocks of code
+//"else and  if"  allows computer to choose between three or more blocks of code conditions
+//"else if" always requires condition
+//"else" never requires a condition
+if(kidHeight > minHeight){
+    //code performed if condition is true
+    console.log("You can ride the Coaster");
+}else if(kidHeight > wParentHeight){
+//code performed only if first if condition is false
+//you can ride with a parent present
+    //never have a conditional attached to else
+    console.log("Sorry kid you got some growing to do");
+}else{
+//printout sorry you have growing ot do
+    console.log("Sorry kid you got some growing to do");
+}*/
+
+
+//Conditional Logic Logic Operators
+
+var budget = 300;
+var iPhonePrice = 199.99;
+var payCheck = 200;
+//if the price of the phone is less than our budget
+if(iPhonePrice < budget && payCheck > 300){
+    console.log("We can buy the phone!");
+}else{
+    console.log("No phone for you!!");
+}
+//if(){
+    //console.log("we can buy the phone");
+//}else{
+   // console.log("no phone 4 u");
+//}
