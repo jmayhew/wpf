62ee260490b36fc7fbeaa267da19073b0462a883
jmayhew <jessemayhew@fullsail.edu>
2013-10-12 12:05:03 -0400

    conditionals

---
+++ week three conditionals/js/conditionals.js
@@ -0,0 +1,12 @@
+/**
+ * Created by jessemayhew on 10/12/13.
+ */
+//conditionals
+var oldEnough = true;
+
+//if the child is old enough print to the console "you can ride"
+if(oldEnough){
+    //code performed if condition is true
+  console.log("You can ride the Coaster");
+
+}
