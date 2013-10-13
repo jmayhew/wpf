19e3c8ddd68c9dd743c127747d4b14fc3989878c
jmayhew <jessemayhew@fullsail.edu>
2013-10-13 13:43:28 -0400

    conditionals worksheet

---
+++ Conditionals_worksheet/js/conditionals.js
@@ -0,0 +1,11 @@
+/**
+ * Created by jessemayhew on 10/13/13.
+ */
+var contestantsWeight = 4;
+
+
+if(contestantsWeight >= 250){
+    console.log("You can join the heavyweight division");
+}else{
+    console.log("You need to gain some weight first");
+}
