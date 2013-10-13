1b56494db43e881d4b59dd093fe052451c4781bd
jmayhew <jessemayhew@fullsail.edu>
2013-10-13 14:35:49 -0400

    conditionals worksheet

--- Conditionals_worksheet/js/conditionals.js
+++ Conditionals_worksheet/js/conditionals.js
@@ -1,11 +1,21 @@
 /**
  * Created by jessemayhew on 10/13/13.
  */
-var contestantsWeight = 4;
+/*var contestantsWeight = 4;
 
 
 if(contestantsWeight >= 250){
     console.log("You can join the heavyweight division");
 }else{
     console.log("You need to gain some weight first");
+}*/
+
+measurement =prompt("What is the measurement you need converted?(Please use F or C only no number yet)");
+temp = prompt("What is the degree you need converted?(Please state number only at this point)");
+if(measurement === "C"){
+    temp*9/5+32;
+    console.log("Your conversion is "+temp+"F");
+}else{
+    (measurement === "F" && (temp-32)*5/9);
+    console.log("Your conversion is "+temp+"C");
 }
--- week two conditionals/js/conditionals.js
+++ week two conditionals/js/conditionals.js
@@ -4,33 +4,50 @@
 //conditionals
 
 //if the kid is over 48 inches in height
-/*var kidHeight = 50;
+/*
+
+var kidHeight = 50;
 var minHeight = 48;
 var sneakerLift = 2;
+
 //if the child is old enough print to the console "you can ride"
+
 if(kidHeight+sneakerLift > minHeight ){
+
     //code performed if condition is true
+
   console.log("You can ride the Coaster");
 
 }*/
+
 //console.log("this comes after when false");
-/*var kidHeight = 47;
+/*
+
+var kidHeight = 47;
 var minHeight = 48;
+
 //if the child is old enough print to the console
 //if the child is over 48 inches in height
+
 if(kidHeight > minHeight){
+
     //code performed if condition is true
+
     console.log("You can ride the Coaster");
 }else{
+
 //substituting if(kidHeight <= minHeight){
+
     console.log("Sorry kid you got some growing to do");
 }*/
 
 /*
 //conditional logic else if
+
 var kidHeight = 47;
 var minHeight = 48;
 var wParentHeight = 45;//the height of the kid w/parent
+
 //if the child is old enough print to the console
 //if the child is over 48 inches in height
 //plain if statements are for the computer to choose between doing a block of code or to skip it
@@ -38,16 +55,23 @@ var wParentHeight = 45;//the height of the kid w/parent
 //"else and  if"  allows computer to choose between three or more blocks of code conditions
 //"else if" always requires condition
 //"else" never requires a condition
+
 if(kidHeight > minHeight){
+
     //code performed if condition is true
+
     console.log("You can ride the Coaster");
 }else if(kidHeight > wParentHeight){
+
 //code performed only if first if condition is false
 //you can ride with a parent present
     //never have a conditional attached to else
+
     console.log("Sorry kid you got some growing to do");
 }else{
-//printout sorry you have growing ot do
+
+//printout sorry you have growing up to do
+
     console.log("Sorry kid you got some growing to do");
 }*/
 
@@ -56,10 +80,14 @@ if(kidHeight > minHeight){
 
 //And
 
-/*var budget = 300;
+/*
+
+var budget = 300;
 var iPhonePrice = 199.99;
 var payCheck = 200;
+
 //if the price of the phone is less than our budget
+
 if(iPhonePrice < budget && payCheck > 300){
     console.log("We can buy the phone!");
 }else{
@@ -71,15 +99,20 @@ if(iPhonePrice < budget && payCheck > 300){
    // console.log("no phone 4 u");
 //}*/
 
-/*var budget = 300;
+/*
+
+var budget = 300;
 var iPhonePrice = 199.99;
 var wonLottery = true;
+
 //if the price of the phone is less than our budget
+
 if(iPhonePrice < budget || wonLottery === true){
     console.log("We can buy the phone!");
 }else{
     console.log("No phone for you!!");
 }
+
 //if(){
 //console.log("we can buy the phone");
 //}else{
@@ -93,10 +126,13 @@ if(iPhonePrice < budget || wonLottery === true){
 }else{
     do if false;
 }
+
 //Shortening as ternary operation
 
 //(condition) ? do if true : do if false;
+
 //var gpa = 48;
+
 //if gpa is over the min 2.0 score, the student can graduate
 
 /*if( gpa > 2.0){
@@ -111,6 +147,7 @@ var age = 11;
 var book;
 
 //if the child is under 10 , they get green eggs and ham, otherwise they get hg wells the time machine
+
 //if(age < 10){
    // book = "green eggs";
 //}else{
@@ -119,5 +156,6 @@ var book;
 //console.log(book);
 
 //above problem as ternary
+
 book = (age <10)? "green eggs":"time machine";
 console.log(book);
