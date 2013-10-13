1a03b62b37c79e0f2a5fa8b83c0f1786aaffc9cd
jmayhew <jessemayhew@fullsail.edu>
2013-10-13 16:52:13 -0400

    temperature conversion

--- Conditionals_worksheet/js/conditionals.js
+++ Conditionals_worksheet/js/conditionals.js
@@ -10,12 +10,12 @@ if(contestantsWeight >= 250){
     console.log("You need to gain some weight first");
 }*/
 
-measurement =prompt("What is the measurement you need converted?(Please use F or C only no number yet)");
+measurement =prompt("Do you need to find F or C?(Please enter an F or a C");
 temp = prompt("What is the degree you need converted?(Please state number only at this point)");
 if(measurement === "C"){
-    temp*9/5+32;
-    console.log("Your conversion is "+temp+"F");
+    result=(temp*9/5+32);
+    console.log("Your conversion is "+result+"F");
 }else{
-    (measurement === "F" && (temp-32)*5/9);
-    console.log("Your conversion is "+temp+"C");
+    result=(measurement === "F" && (temp-32)*5/9);
+    console.log("Your conversion is "+result+"C");
 }
