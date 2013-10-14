3f47a07093a38865d261bd85b3e939694dd9d9fc
jmayhew <jessemayhew@fullsail.edu>
2013-10-13 20:06:52 -0400

    grades worksheet problem

--- Conditionals_worksheet/js/conditionals.js
+++ Conditionals_worksheet/js/conditionals.js
@@ -10,7 +10,7 @@ if(contestantsWeight >= 250){
     console.log("You need to gain some weight first");
 }*/
 
-measurement =prompt("Do you need to find F or C?(Please enter an F or a C");
+/*measurement =prompt("Do you need to find F or C?(Please enter an F or a C");
 temp = prompt("What is the degree you need converted?(Please state number only at this point)");
 if(measurement === "C"){
     result=(temp*9/5+32);
@@ -18,4 +18,16 @@ if(measurement === "C"){
 }else{
     result=(measurement === "F" && (temp-32)*5/9);
     console.log("Your conversion is "+result+"C");
-}
+}*/
+
+var grade = prompt("What is your grade?");
+
+    if(grade <= 69){
+        console.log("You failed miserably");
+    }else if(grade>=70 && grade<=80){
+        console.log("You barely passed with a D");
+    }else if(grade>=81 && grade <= 90){
+        console.log("You are average");
+    }else{
+    console.log("you are a genius");
+   }
