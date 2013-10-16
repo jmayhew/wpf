1b3b207307fcfd975bc559acbbdf9d26c37245fe
jmayhew <jessemayhew@fullsail.edu>
2013-10-16 10:03:09 -0400

    personal comments

--- Conditionals_Personal/js/conditionals_personal.js
+++ Conditionals_Personal/js/conditionals_personal.js
@@ -3,6 +3,6 @@
  */
 //Ternary with amount of time working on homework
 
-var homeworkTime = prompt("How many minutes have you been working at your studies?");
-breakTime=(homeworkTime < 60)?"Keep going" : "Take a snack break now";
-    console.log(breakTime);
+var homeworkTime = prompt("How many minutes have you been working at your studies?");//Prompting the student to enter the minutes of time he has been studying
+breakTime=(homeworkTime < 60)?"Keep going" : "Take a snack break now";//Ternary if statement saying if it is true Keep Going if it is false take a snack break
+    console.log(breakTime);//logs the output of the statement
