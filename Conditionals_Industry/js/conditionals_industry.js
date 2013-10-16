62edc36bee5368987081e19a2f28d9b88f6d8e19
jmayhew <jessemayhew@fullsail.edu>
2013-10-15 20:42:49 -0400

    commenting

--- Conditionals_Industry/js/conditionals_industry.js
+++ Conditionals_Industry/js/conditionals_industry.js
@@ -5,10 +5,10 @@
     var wholeTomatoes = prompt("How many tomatoes do you have?")//Prompting the user to count the tomatoes and enter the amount
 
 
-if(wholeTomatoes<40){
-    console.log("Manager must order more tomatoes or go the store");
-}else{
-    console.log("You can wait a while to order tomatoes.");
+if(wholeTomatoes<40){//if whole tomatoes are less than 40 manager must order some more
+    console.log("Manager must order more tomatoes or go the store");//console log tells the manger what to do
+}else{//else block
+    console.log("You can wait a while to order tomatoes.");//if the if statement is false console logs to the manager he has time to order another batch
 }
 
 
