2319ce25f84c6afb09b621c5ce58c45a5d3b17d0
jmayhew <jessemayhew@fullsail.edu>
2013-10-15 20:38:41 -0400

    conditionals industry

---
+++ Conditionals_Industry/js/conditionals_industry.js
@@ -0,0 +1,14 @@
+/**
+ * Created by jessemayhew on 10/15/13.
+ */
+//Industry Restaurant Inventory
+    var wholeTomatoes = prompt("How many tomatoes do you have?")
+
+
+if(wholeTomatoes<40){
+    console.log("Manager must order more tomatoes or go the store");
+}else{
+    console.log("You can wait a while to order tomatoes.");
+}
+
+
