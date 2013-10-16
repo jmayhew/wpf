284fc206b5875beb8a9fc98134816ce4b52da8ff
jmayhew <jessemayhew@fullsail.edu>
2013-10-16 10:08:13 -0400

    wacky comments

--- Conditionals_Wacky/js/conditionals_wacky.js
+++ Conditionals_Wacky/js/conditionals_wacky.js
@@ -12,15 +12,15 @@ if(gravity <= 3.724){//if gravity is less than or equal to 3.724
      console.log("You are close to Mars");//console log guesses he may be close to Mars
  }else if(gravity >= 3.725 && gravity<=8.82){//else if condition with and operator
     console.log("You are close to being on Venus");//console logs guesses he may be close Venus
-}else if(gravity>=8.83 && gravity<= 8.918){//else if condition with and operator
+}else if(gravity>=8.83 && gravity<= 8.918){//else if condition with and operator if both are true the user is close to being on the planet Saturn
     console.log("You may be on Saturn");//console log guesses he may be on Saturn
-}else if(gravity>=8.918 && gravity<= 9.8){//else if condition with and operator
+}else if(gravity>=8.918 && gravity<= 9.8){//else if condition with and operator if both are true the user is close to being on the planet Earth
     console.log("You are probably on Earth");//console log guesses he is probably on Earth
-}else if(gravity>=9.9 && gravity <= 11.1172){
-    console.log("You are probably near Neptune");
+}else if(gravity>=9.9 && gravity <= 11.1172){//else if condition with and operator if both are true the user is close to being on the planet Neptune
+    console.log("You are probably near Neptune");//console log guesses he is probably on
 
-}else{
-    console.log("You have entered a black hole and found another universe");
-}
+}else{//else block the last possibility
+    console.log("You have entered a black hole and found another universe");//outlandish amount of gravity user has entered and gives a string theorist answer
+}//ends conditional
 
 
