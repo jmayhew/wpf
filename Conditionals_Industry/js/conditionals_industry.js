f47efb11220d2846c9d7ddaa2a351065f86beb13
jmayhew <jessemayhew@fullsail.edu>
2013-10-15 20:40:19 -0400

    commenting

--- Conditionals_Industry/js/conditionals_industry.js
+++ Conditionals_Industry/js/conditionals_industry.js
@@ -2,7 +2,7 @@
  * Created by jessemayhew on 10/15/13.
  */
 //Industry Restaurant Inventory
-    var wholeTomatoes = prompt("How many tomatoes do you have?")
+    var wholeTomatoes = prompt("How many tomatoes do you have?")//Prompting the user to count the tomatoes and enter the amount
 
 
 if(wholeTomatoes<40){
