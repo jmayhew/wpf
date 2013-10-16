b10aaf789f85f5e98e8c26da9eb31125e712d5f1
jmayhew <jessemayhew@fullsail.edu>
2013-10-16 10:10:06 -0400

    more wacky comments

--- Conditionals_Wacky/js/conditionals_wacky.js
+++ Conditionals_Wacky/js/conditionals_wacky.js
@@ -8,7 +8,7 @@
 
 var gravity=prompt("What is the gravity in meters per second where you are so we can find out what planet you landed on?");//prompts the end user to enter a number for gravity in meters per second to find out where he could be
 
-if(gravity <= 3.724){//if gravity is less than or equal to 3.724
+if(gravity <= 3.724){//if gravity is less than or equal to 3.724 then user may be on the planet Mars
      console.log("You are close to Mars");//console log guesses he may be close to Mars
  }else if(gravity >= 3.725 && gravity<=8.82){//else if condition with and operator
     console.log("You are close to being on Venus");//console logs guesses he may be close Venus
@@ -17,7 +17,7 @@ if(gravity <= 3.724){//if gravity is less than or equal to 3.724
 }else if(gravity>=8.918 && gravity<= 9.8){//else if condition with and operator if both are true the user is close to being on the planet Earth
     console.log("You are probably on Earth");//console log guesses he is probably on Earth
 }else if(gravity>=9.9 && gravity <= 11.1172){//else if condition with and operator if both are true the user is close to being on the planet Neptune
-    console.log("You are probably near Neptune");//console log guesses he is probably on
+    console.log("You are probably near Neptune");//console log guesses he is probably on Neptune
 
 }else{//else block the last possibility
     console.log("You have entered a black hole and found another universe");//outlandish amount of gravity user has entered and gives a string theorist answer
