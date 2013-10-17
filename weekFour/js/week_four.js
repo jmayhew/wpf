ffcfe9ed5f693f323ae9a8defd81c0d216e8a584
jmayhew <jessemayhew@fullsail.edu>
2013-10-17 10:36:49 -0400

    week four screencasts

---
+++ weekFour/js/week_four.js
@@ -0,0 +1,47 @@
+/**
+ * Created by jessemayhew on 10/17/13.
+ */
+//Loops
+
+//normal if statement
+
+/*var b = 10;
+
+if (b > 0 ){
+    console.log(b);
+}*/
+
+//while loop
+
+/*var b = 10;
+
+while (b > 0 ){
+    console.log(b);
+//decrement it
+     b--;//could also use ++ (increment
+}*/
+
+//adding concatenation
+
+var b = 10;// sets up the index
+
+while (b > 0 ){//checks the condition
+    console.log(b + 'kegs on the wall');//concatenation
+    b--;// decrements  or increments the index
+}
+
+//DO While loop (not as common as loop
+
+/*var c = 10;
+
+do{// run this code
+    console.log(c + 'Barrels of apples');
+    c--;
+}
+while(c > 0);// while our condition is true*/
+
+//For Loop - shorthand version of loops , culmination of all the loops put together
+
+for (var i = 10; i > 0; i--){// sets up condition in on line decrements and variable and condition
+    console.log(i + 'barrels of oranges')//if true lists each one in index
+}
