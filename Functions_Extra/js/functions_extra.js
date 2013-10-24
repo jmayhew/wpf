123db2b3dd3da99eed21710477567541316fa042
jmayhew <jessemayhew@fullsail.edu>
2013-10-24 11:40:48 -0400

    More of functions extra problem

--- Functions_Extra/js/functions_extra.js
+++ Functions_Extra/js/functions_extra.js
@@ -2,32 +2,32 @@
  * Created by jessemayhew on 10/23/13.
  */
 
-//Going on a diet Functions_Personal
+//Going on a diet Function_Extra and Volume of a fish tank problem in the personal category
 
+
+/*
 
 var time = prompt("What time is it in military time?(Enter the hours and minutes in double digits)");//variable prompting the user for time
 
 dinner = (time > 2000)? "I can not have dinner":"I can have dinner";//Ternary with conditional ternary
     console.log(dinner);// logs the value dinner
 
+ */
+
+//Calculates volume of a rectangle for a fish tank
+
+function calcVolume(){//normal function with curly braces to start function
+    var width= 10;//variable for width
+    var height= 19;//variable for height
+    var length= 24;// variable for length
+    var volume = width*height*length;//expression with arithmetic operators
+    console.log("The volume of your rectangle is "+volume);// logs the string plus volume
+    }// ends function
+
+calcVolume();// calls or invokes the function calcVolume
+calcVolume();// calls it twice making the code reusable
 
 
 
-
-
-
-
-
-
-
-function calcArea(){//curly braces say these are the codes my function is going ot run
-    var width = 20;
-    var height = 30;
-    var area = width * height;
-    console.log(area);
-}
-
-calcArea();//invocation of function- automatically calls "area" , function call, execution of function
-calcArea();//twice if necessary- making the code REUSABLE*/
 
 
